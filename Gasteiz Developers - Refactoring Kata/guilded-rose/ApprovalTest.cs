03900cb20860de6d1f402a975316242e9388a667
Jesus Maria Escudero Justel <jmescudero@plainconcepts.com>
2019-09-18 16:22:39 +0200

    Add guilded rose kata

---
+++ Gasteiz Developers - Refactoring Kata/guilded-rose/ApprovalTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace csharp
+{
+    [TestFixture]
+    public class ApprovalTest
+    {
+        [Test]
+        public void ThirtyDays()
+        {
+            var lines = File.ReadAllLines("C:\\Dev\\MyProjects\\talks-and-presentations\\Gasteiz Developers - Refactoring Kata\\guilded-rose\\ThirtyDays.txt");
+
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\r\n"));
+
+            Program.Main(new string[] { });
+            String output = fakeoutput.ToString();
+
+            var outputLines = output.Split('\n');
+            for (var i = 0; i < Math.Min(lines.Length, outputLines.Length); i++)
+            {
+                outputLines[i] = outputLines[i].Replace("\r", string.Empty);
+                Assert.AreEqual(lines[i], outputLines[i]);
+            }
+        }
+    }
+}
