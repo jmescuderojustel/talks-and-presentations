77b4b4910f1a1e1dee5d1e96b778b14cc9e1f9e8
Jesus Maria Escudero Justel <jmescudero@plainconcepts.com>
2019-09-18 16:24:47 +0200

    Create FACT content for xUnit presentation

---
+++ Plain Concepts - PlainTV - xUnit/PokemonLeague.Tests/PokemonShould_Fact.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+
+namespace PokemonLeague.Tests
+{
+    public class FactVersionPokemonShould
+    {
+        [Fact]
+        public void CharmanderFightAgainstPikatxu()
+        {
+            var charmander = new Pokemon
+            {
+                Name = "Charmander",
+                Life = 100,
+                Strength = 20
+            };
+
+            var pikatxu = new Pokemon
+            {
+                Name = "Pikatxu",
+                Life = 80,
+                Strength = 15
+            };
+
+            charmander.FightAgainst(pikatxu);
+
+            Assert.Equal(85, charmander.Life);
+            Assert.Equal(60, pikatxu.Life);
+        }
+
+        [Fact]
+        public void CharizardFightAgainstSquirtle()
+        {
+            var charizard = new Pokemon
+            {
+                Name = "Charizard",
+                Life = 90,
+                Strength = 40
+            };
+
+            var squirtle = new Pokemon
+            {
+                Name = "Squirtle",
+                Life = 90,
+                Strength = 10
+            };
+
+            charizard.FightAgainst(squirtle);
+
+            Assert.Equal(80, charizard.Life);
+            Assert.Equal(50, squirtle.Life);
+        }
+    }
+}
---
+++ Plain Concepts - PlainTV - xUnit/PokemonLeague/Pokemon.cs
@@ -0,0 +1,15 @@
+﻿namespace PokemonLeague
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Life { get; set; }
+
+        public void FightAgainst(Pokemon otherPokemon)
+        {
+            otherPokemon.Life -= Strength;
+            Life -= otherPokemon.Strength;
+        }
+    }
+}
